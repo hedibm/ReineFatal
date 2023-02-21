d35e93101ebaefc2940fdbad44fe3bbe3d8af860
farahbelali <fafybelali@gmail.com>
2023-02-21 12:14:25 +0100

    crud evenement offre

---
+++ Event/src/Main/main.java
@@ -0,0 +1,24 @@
+package Main;
+
+import Entities.event;
+import Tools.MaConnexion;
+import java.util.Date;
+import java.time.LocalDate;
+import java.time.Month;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MaConnexion mc = MaConnexion.getInstance();
+
+     }
+    }
+    
+
